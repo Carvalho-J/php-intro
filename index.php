dc42a4b1d149a0931d08b3f09969d7de880dedb3
jean carvalho <jean.carvalho1379@gmail.com>
2020-02-23 00:23:01 -0300

    segunda parte aula

--- index.php
+++ index.php
@@ -10,5 +10,10 @@ $categorias[] = 'infantil'; //categorias acrescentadas ao array
 $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
 
+//print_r($categorias);
 
-print_r($categorias);
+$nome = 'Jean';
+$idade = 25;
+
+var_dump($nome);
+var_dump($idade);
